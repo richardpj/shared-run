eca7ff9605ccd4fd0a9267b933751fdec268b8b1
Richard Pierce-Jones <richard.pj@entelect.co.za>
2016-09-05 16:48:16 +0200

    triggering travis ci

--- test/findDeps.test.js
+++ test/findDeps.test.js
@@ -5,9 +5,11 @@ var findDeps = require('../main').findDeps,
 
 
 describe('#findDeps', function () {
+
     it('should return an empty array when no package.json is found.', function () {
         findDeps('./test/target').should.deep.equal([]);
     });
+
     it('should return the correct local dependencies when invoked with no switches.', function () {
         findDeps('./test/target/module-a')
             .should.deep.equal
@@ -17,6 +19,7 @@ describe('#findDeps', function () {
                 path.resolve('./test/target/module-b')
             ]);
     });
+
     it('should also scan devDependencies when the -dev switch is specified.', function () {
         findDeps('./test/target/module-a', false, true)
             .should.deep.equal
@@ -27,6 +30,7 @@ describe('#findDeps', function () {
                 path.resolve('./test/target/module-b')
             ]);
     });
+
     it('should ONLY scan devDependencies when the -noProd and -dev swithes are specified.', function () {
         findDeps('./test/target/module-a', true, true)
             .should.deep.equal
@@ -34,6 +38,7 @@ describe('#findDeps', function () {
                 path.resolve('./test/target/dev-dep'),
             ]);
     });
+    
     it('should apply the comma delimited string filter option.', function () {
         findDeps('./test/target/module-a', false, true, 'another-dep,dev-dep')
             .should.deep.equal
